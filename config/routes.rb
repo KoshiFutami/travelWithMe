9f7a8fd250f7c2f932b634754faf521f6bed8a5e
Koshi Futami <koshi.futami@gmail.com>
2020-05-14 20:42:21 +0900

    Add bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
+  resources :articles do
+  end
   root 'articles#index'
-  get 'articles/index'
-  get 'articles/show'
-  get 'articles/edit'
-  get 'articles/new'
-  get 'articles/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
