d018abc271f1bb856d7477598b307a0c34f3a8bb
Koshi Futami <koshi.futami@gmail.com>
2020-05-18 00:03:40 +0900

    Add some styling for article index page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,8 @@
 class ArticlesController < ApplicationController
+  before_action :find_article, only: [:show, :edit, :update, :destroy]
+
   def index
+    @articles = Article.all.order('created_at DESC')
   end
 
   def show
@@ -8,9 +11,42 @@ class ArticlesController < ApplicationController
   def edit
   end
 
+  def update
+    if @article.update(article_params)
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
+  end
+
   def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to articles_path
+    else
+      render 'new'
+    end
   end
 
   def destroy
+    @article.destroy
+    redirect_to articles_path
   end
+
+
+  private
+
+  def find_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
+
 end
